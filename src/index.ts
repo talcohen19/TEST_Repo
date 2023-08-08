d60170907160e3e6078ce589c47d857bc2caf89c
Tal-Class <Tal-Class>
2023-08-08 19:29:31 +0300

    second commit

--- src/index.ts
+++ src/index.ts
@@ -12,6 +12,7 @@ AppDataSource.initialize().then(async () => {
     app.use(bodyParser.json())
 
     // register express routes from defined application routes
+    //bla bla bla
     Routes.forEach(route => {
         (app as any)[route.method](route.route, (req: Request, res: Response, next: Function) => {
             const result = (new (route.controller as any))[route.action](req, res, next)
